af6b2d14675149ebc69685911bc53f5b853c8181
Dave Kanter <moatgoat@hotmail.com>
2019-07-20 20:28:52 -0700

    Finished!  And everything more-or-less works."

--- README.md
+++ README.md
@@ -28,6 +28,9 @@ yarn run lint
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
+# Following Tutorial
+[Getting Started With Vue](https://www.taniarascia.com/getting-started-with-vue/) by Tania Rascia
+
 # Interesting things I learned!
 
 A `.vue` file always consists of three things: 
@@ -50,4 +53,7 @@ Example:
      <style scoped></style>
 
 The data and logic for the component goes into `<script>` but only `name` is required.  The `<style>` tag is just CSS.  'scoped' means it only applys to this component and not globally.
+
+# Verdict
+Success!  I wish all tutorials were like this one.  Easy to follow, and works 100% without my having to spend two hours on Stack trying to figure out why nothing works.
 
--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,10 @@
     },
     data() { // Data is like state in React
       return {
-        employees: [
+        // Let's do it with REST instead!
+        employees: []
+
+        /*employees: [
           {
             id: 1,
             name: 'The Godfather',
@@ -51,32 +54,90 @@
             name: 'Buttiful Pancake',
             email: 'buttiful@pancake.com',
           },
-        ]
+        ]*/
       }
     },
-    methods: {
-      addEmployee(employee) {
-        const lastId = 
-        this.employees.length > 0
-          ? this.employees[this.employees.length - 1].id
-          : 0
-        const id = lastId + 1
-        const newEmployee = { ...employee, id }
-
-        this.employees = [...this.employees, newEmployee]
-      },
-      deleteEmployee(id) {
-        this.employees = this.employees.filter(
-          employee => employee.id !== id
-        )
-      },
-      editEmployee(id, updatedEmployee) {
-        this.employees = this.employees.map(employee =>
-          employee.id === id ? updatedEmployee : employee
-        )
-      },
+    mounted() { // lifecycle method, equivalent of React's ComponentDidMount
+      this.getEmployees()
     },
-  }
+    methods: {
+      // LET'S DO IT WITH REST!
+    //   addEmployee(employee) {
+    //     const lastId = 
+    //     this.employees.length > 0
+    //       ? this.employees[this.employees.length - 1].id
+    //       : 0
+    //     const id = lastId + 1
+    //     const newEmployee = { ...employee, id }
+
+    //     this.employees = [...this.employees, newEmployee]
+    //   },
+    //   deleteEmployee(id) {
+    //     this.employees = this.employees.filter(
+    //       employee => employee.id !== id
+    //     )
+    //   },
+    //   editEmployee(id, updatedEmployee) {
+    //     this.employees = this.employees.map(employee =>
+    //       employee.id === id ? updatedEmployee : employee
+    //     )
+    //   },
+    // },
+    // GET: retrieve a resource
+      async getEmployees() {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users')
+          const data = await response.json()
+          this.employees = data
+        } catch (error) {
+          // eslint-disable-next-line
+          console.error(error)
+        }
+      },
+      //POST: create a new resource (non-idempotent)
+      async addEmployee(employee) {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/users', {
+            method: 'POST',
+            body: JSON.stringify(employee),
+            headers: { 'Content-type': 'application/json; charset=UTF-8'},
+          })
+          const data = await response.json()
+          this.employees = [...this.employees, data]
+        } catch (error) {
+          // eslint-disable-next-line
+          console.error(error)
+        }
+      },
+      //PUT: Update an existing resource (idempotent)
+      async editEmployee(id, updatedEmployee) {
+        try {
+          const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(updatedEmployee),
+            headers: { 'Content-type': 'application/json; charset=UTF-8' }
+          })
+          const data = await response.json()
+          this.employees = this.employees.map(employee => (employee.id === id ? data : employee))
+        } catch (error) {
+           // eslint-disable-next-line
+          console.error(error)
+        }
+      },
+      //DELETE: Remove an existing resource
+      async deleteEmployee(id) {
+        try {
+          await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+            method: "DELETE"
+          })
+          this.employees = this.employees.filter(employee => employee.id !== id)
+        } catch (error) {
+           // eslint-disable-next-line
+          console.error(error)
+        }
+      }
+    }, // methods
+  } // export default
 
 </script>
 
